4ccc9b68b73f9c5569a7c90871cac6aae0990c2e
kfcampbell <kfcampbell@github.com>
2020-10-04 19:59:57 -0700

    Scoring formatted

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/kfcampbell/pigskin/clients/fleaflicker"
 	"github.com/kfcampbell/pigskin/responses"
 )
+
+const topNScorers = 3
 
 func main() {
 	if err := realMain(); err != nil {
@@ -26,6 +29,21 @@ func realMain() error {
 		fmt.Println(score)
 	}
 
+	biggestWin := getBiggestWin(scores.Games)
+	fmt.Println(biggestWin)
+
+	topScorers, err := getTopScorers(scores.Games)
+	if err != nil {
+		return err
+	}
+	fmt.Println(topScorers)
+
+	bottomScorers, err := getBottomScorers(scores.Games)
+	if err != nil {
+		return err
+	}
+	fmt.Println(bottomScorers)
+
 	return nil
 }
 
@@ -43,4 +61,97 @@ func formatScore(game responses.FantasyGame) string {
 	} else {
 		return fmt.Sprintf("Whaaaat....%v and %v tied with a score of %v-%v.\n", homeTeam, awayTeam, homeScore, awayScore)
 	}
+}
+
+func getBiggestWin(games []responses.FantasyGame) string {
+	biggestWin := games[0]
+	difference := getDifferenceInScore(games[0])
+	for i := 1; i < len(games); i++ {
+		currDifference := getDifferenceInScore(games[i])
+		if currDifference > difference {
+			difference = currDifference
+			biggestWin = games[i]
+		}
+	}
+	if isHomeWinning(biggestWin) > 0 {
+		return fmt.Sprintf("Biggest win: %v over %v, %v-%v (difference of %v points).",
+			biggestWin.Home.Name, biggestWin.Away.Name, biggestWin.HomeScore.Score.Value, biggestWin.AwayScore.Score.Value, difference)
+	}
+	return fmt.Sprintf("Biggest win: %v over %v, %v-%v (difference of %v points).",
+		biggestWin.Away.Name, biggestWin.Home.Name, biggestWin.AwayScore.Score.Value, biggestWin.HomeScore.Score.Value, difference)
+}
+
+func getTopScorers(games []responses.FantasyGame) (string, error) {
+	topScorers, err := NewScorers(true, topNScorers)
+	if err != nil {
+		return "Error getting top scorers", fmt.Errorf("error getting top scorers: %v", err)
+	}
+
+	for i := 0; i < len(games); i++ {
+		homeScorer := GetScorer(true, games[i])
+		awayScorer := GetScorer(false, games[i])
+
+		if topScorers.ShouldAddScorer(*homeScorer) {
+			topScorers.AddScorer(*homeScorer)
+		}
+		if topScorers.ShouldAddScorer(*awayScorer) {
+			topScorers.AddScorer(*awayScorer)
+		}
+	}
+
+	formatted := "Top scorers: "
+	for i := 0; i < topScorers.length; i++ {
+		formatted += fmt.Sprintf("%v (%v points), ", topScorers.scorers[i].Team.Name, topScorers.scorers[i].Score)
+	}
+	formatted = strings.TrimSuffix(formatted, ", ")
+
+	return formatted, nil
+}
+
+func getBottomScorers(games []responses.FantasyGame) (string, error) {
+	bottomScorers, err := NewScorers(false, topNScorers)
+	if err != nil {
+		return "Error getting top scorers", fmt.Errorf("error getting top scorers: %v", err)
+	}
+
+	for i := 0; i < len(games); i++ {
+		homeScorer := GetScorer(true, games[i])
+		awayScorer := GetScorer(false, games[i])
+
+		if bottomScorers.ShouldAddScorer(*homeScorer) {
+			bottomScorers.AddScorer(*homeScorer)
+		}
+		if bottomScorers.ShouldAddScorer(*awayScorer) {
+			bottomScorers.AddScorer(*awayScorer)
+		}
+	}
+
+	formatted := "Bottom scorers: "
+	for i := 0; i < bottomScorers.length; i++ {
+		formatted += fmt.Sprintf("%v (%v points), ", bottomScorers.scorers[i].Team.Name, bottomScorers.scorers[i].Score)
+	}
+	formatted = strings.TrimSuffix(formatted, ", ")
+
+	return formatted, nil
+}
+
+// isHomeWinning returns 1 if true, -1 if false, and 0 if tied
+func isHomeWinning(game responses.FantasyGame) int {
+	homeScore := game.HomeScore.Score.Value
+	awayScore := game.AwayScore.Score.Value
+
+	if homeScore > awayScore {
+		return 1
+	} else if awayScore > homeScore {
+		return -1
+	} else {
+		return 0
+	}
+}
+
+func getDifferenceInScore(game responses.FantasyGame) float32 {
+	if isHomeWinning(game) > 0 {
+		return game.HomeScore.Score.Value - game.AwayScore.Score.Value
+	}
+	return game.AwayScore.Score.Value - game.HomeScore.Score.Value
 }
---
+++ scorers.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kfcampbell/pigskin/responses"
+)
+
+// Scorer is a convenience type for keeping track of a week's high- and low-scorers
+type Scorer struct {
+	Team  responses.Team
+	Score float32
+}
+
+// GetScorer returns a home or away scorer from a given game
+func GetScorer(home bool, game responses.FantasyGame) *Scorer {
+	if home {
+		return &Scorer{
+			Team:  game.Home,
+			Score: game.HomeScore.Score.Value,
+		}
+	}
+
+	return &Scorer{
+		Team:  game.Away,
+		Score: game.AwayScore.Score.Value,
+	}
+}
+
+// Scorers provides a way to track and order the top or bottom N scorers.
+// No order guarantees are made about the Scorers slice
+type Scorers struct {
+	isTop   bool
+	length  int
+	scorers []Scorer
+}
+
+// NewScorers creates and returns a new Scorers struct
+func NewScorers(isTop bool, length int) (*Scorers, error) {
+	if length < 1 {
+		return &Scorers{}, fmt.Errorf("cannot initialize Scorers with a length of %v", length)
+	}
+	scorers := make([]Scorer, length)
+
+	if !isTop {
+		for i := 0; i < len(scorers); i++ {
+			scorers[i].Score = 1000
+		}
+	}
+	return &Scorers{
+		isTop:   isTop,
+		length:  length,
+		scorers: scorers,
+	}, nil
+}
+
+// ShouldAddScorer returns true if the given scorer is in the top (or bottom) N scorers
+func (s *Scorers) ShouldAddScorer(scorer Scorer) bool {
+	if len(s.scorers) < s.length {
+		return true
+	}
+
+	if s.isTop {
+		for i := 0; i < len(s.scorers); i++ {
+			if s.scorers[i].Score < scorer.Score {
+				return true
+			}
+		}
+		return false
+	}
+	for i := 0; i < len(s.scorers); i++ {
+		if s.scorers[i].Score > scorer.Score {
+			return true
+		}
+	}
+	return false
+}
+
+// AddScorer adds a given scorer to the slice of N scorers, replacing the previous high- or lowest-rated scorer
+func (s *Scorers) AddScorer(scorer Scorer) {
+	if s.isTop {
+		lowestScoreIndex := 0
+		lowestScore := float32(1000)
+		for i := 0; i < len(s.scorers); i++ {
+			if s.scorers[i].Score < lowestScore {
+				lowestScore = s.scorers[i].Score
+				lowestScoreIndex = i
+			}
+		}
+		s.scorers[lowestScoreIndex] = scorer
+		return
+	}
+	highestScoreIndex := 0
+	highestScore := float32(-1000)
+	for i := 0; i < len(s.scorers); i++ {
+		if s.scorers[i].Score > highestScore {
+			highestScore = s.scorers[i].Score
+			highestScoreIndex = i
+		}
+	}
+	s.scorers[highestScoreIndex] = scorer
+}
