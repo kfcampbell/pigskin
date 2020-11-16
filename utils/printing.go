89c267cb8127abad5c187721882ebe2d80368d4c
kfcampbell <kfcampbell@github.com>
2020-11-15 19:10:58 -0800

    Space out body message a bit

--- utils/printing.go
+++ utils/printing.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/kfcampbell/pigskin/responses"
 )
@@ -28,40 +27,38 @@ func FormatScore(game responses.FantasyGame) string {
 // FormatBiggestWin prettyprints the winner of a fantasy game
 func FormatBiggestWin(biggestWin responses.FantasyGame, difference float32) string {
 	if IsHomeWinning(biggestWin) > -1 {
-		return fmt.Sprintf("- **Biggest win**: _%v_ over _%v_, %.2f-%.2f (difference of %.2f points).",
+		return fmt.Sprintf("- **Biggest win**: \n_%v_ over _%v_, \n%.2f-%.2f (difference of %.2f points).",
 			biggestWin.Home.Name, biggestWin.Away.Name, biggestWin.HomeScore.Score.Value, biggestWin.AwayScore.Score.Value, difference)
 	}
-	return fmt.Sprintf("- **Biggest win**: _%v_ over _%v_, %.2f-%.2f (difference of %.2f points).",
+	return fmt.Sprintf("- **Biggest win**: \n_%v_ over _%v_, \n%.2f-%.2f (difference of %.2f points).",
 		biggestWin.Away.Name, biggestWin.Home.Name, biggestWin.AwayScore.Score.Value, biggestWin.HomeScore.Score.Value, difference)
 }
 
 // FormatClosestGame prettyprints the closest game
 func FormatClosestGame(closestGame responses.FantasyGame, difference float32) string {
 	if IsHomeWinning(closestGame) > 0 {
-		return fmt.Sprintf("- **Closest game**: _%v_ over %v, %.2f-%.2f (difference of %.2f points).", closestGame.Home.Name, closestGame.Away.Name, closestGame.HomeScore.Score.Value, closestGame.AwayScore.Score.Value, difference)
+		return fmt.Sprintf("- **Closest game**: \n_%v_ over %v, \n%.2f-%.2f (difference of %.2f points).", closestGame.Home.Name, closestGame.Away.Name, closestGame.HomeScore.Score.Value, closestGame.AwayScore.Score.Value, difference)
 	}
 
-	return fmt.Sprintf("- **Closest game**: _%v_ over %v, %.2f-%.2f (difference of %.2f points).", closestGame.Away.Name, closestGame.Home.Name, closestGame.AwayScore.Score.Value, closestGame.HomeScore.Score.Value, difference)
+	return fmt.Sprintf("- **Closest game**: \n_%v_ over %v, \n%.2f-%.2f (difference of %.2f points).", closestGame.Away.Name, closestGame.Home.Name, closestGame.AwayScore.Score.Value, closestGame.HomeScore.Score.Value, difference)
 }
 
 // FormatTopScorers prettyprints a given *Scorers object
 func FormatTopScorers(topScorers *Scorers) string {
-	formatted := "- **Top scorers** :"
+	formatted := "- **Top scorers** :\n"
 	for i := 0; i < topScorers.GetLength(); i++ {
-		formatted += fmt.Sprintf("_%v_ (%.2f points), ", topScorers.GetScorers()[i].Team.Name, topScorers.GetScorers()[i].Score)
+		formatted += fmt.Sprintf("_%v_ (%.2f points)\n", topScorers.GetScorers()[i].Team.Name, topScorers.GetScorers()[i].Score)
 	}
-	formatted = strings.TrimSuffix(formatted, ", ")
 
 	return formatted
 }
 
 // FormatBottomScorers prettyprints the bottom scorers
 func FormatBottomScorers(bottomScorers *Scorers) string {
-	formatted := "- **Bottom scorers**: "
+	formatted := "- **Bottom scorers**: \n"
 	for i := 0; i < bottomScorers.GetLength(); i++ {
-		formatted += fmt.Sprintf("_%v_ (%.2f points), ", bottomScorers.GetScorers()[i].Team.Name, bottomScorers.GetScorers()[i].Score)
+		formatted += fmt.Sprintf("_%v_ (%.2f points)\n", bottomScorers.GetScorers()[i].Team.Name, bottomScorers.GetScorers()[i].Score)
 	}
-	formatted = strings.TrimSuffix(formatted, ", ")
 
 	return formatted
 }
